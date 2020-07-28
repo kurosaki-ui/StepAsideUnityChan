3ccfe26ad0c8d37ef8843089afbd5382e5a6f908
kurosaki-ui <p10p030@cis.ac.jp>
2020-07-28 22:09:53 +0900

    2020.7.29 　初めてのコミット

---
+++ Assets/DestroyItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour {
+	private GameObject maincamera;
+
+	// Use this for initialization
+	void Start () {
+		this.maincamera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z  <= maincamera.transform.position.z)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
